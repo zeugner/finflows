44b10fe42d354a12fb082e8b07478f82a588a883
Erza Ar <erzaar@gmail.com>
2024-11-26 10:36:28 +0100

    test_20241126

--- routines/finflowers_EA_plausichecks.R
+++ routines/finflowers_EA_plausichecks.R
@@ -11,9 +11,17 @@ aall=readRDS('data/aall.rds')
 
 
 ######### plausibility checks of data to assure consistency ######### 
-fifelse(aall[..S0.S1.LE._T.2023q4]>0,yes='ok',no='nok',na=NA)
+
+#!todo: if not S12K or S13 sectors are filled when FI = F21+F2M (should be not filled)
+
+aall[!(is.na(aall[F2M+F21...S1M+S11+S12O+S12Q+S124.LE._T.]))]>0
+
+result_nok <- aall[F2M+F21...S1M+S11+S12O+S12Q+S124.LE._T.]>0
+
+
 
 #!todo: include a check if S0 is zero also  check if S1 is zero or NA (should be not filled)
+fifelse(aall[..S0.S1.LE._T.2023q4]>0,yes='ok',no='nok',na=NA)
 aall[.AT.S1..LE._T.2023q4]
 #!todo: if not S12K or S13 sectors are filled when FI = F21+F2M (should be not filled)
 #!todo: check if they are NA or not filled or zero before filling with 0 (rule 3+2) (should be not filled)
