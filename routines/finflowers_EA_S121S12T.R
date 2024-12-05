03fa57839bbebaa7796e329dee96a931c5e29651
Erza Ar <erzaar@gmail.com>
2024-12-05 09:56:25 +0100

    20241205

---
+++ routines/finflowers_EA_S121S12T.R
@@ -0,0 +1,11 @@
+#source('https://s-ecfin-web.net1.cec.eu.int/directorates/db/u1/R/routines/installMDecfin.txt')
+
+
+library(MDecfin)
+#setwd('U:/Topics/Spillovers_and_EA/flowoffunds/finflows2024/gitcodedata')
+#setwd('C:/Users/aruqaer/R/finflow/gitclone/finflows')
+
+# Load the previously created matrix with all financial instruments data
+aall=readRDS('data/aall2.rds')
+
+
