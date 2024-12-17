aa3cc122c3f396c5553070312789561360179b2f
Erza Ar <erzaar@gmail.com>
2024-12-17 17:00:13 +0100

    calculated S12R

--- routines/002_finflowers_combineQSA.R
+++ routines/002_finflowers_combineQSA.R
@@ -148,13 +148,15 @@ for (i in irest) {
   }
 }
 
-### stetting order to sectors and calculating RoW (S2) - saving as aall1
+### stetting order to sectors and calculating RoW (S2) + financial sector excluding S12K
+# saving as aall1
 dimnames(aall)[[3]]
 dim(aall)
 
 # set order for sectors 
-sorder=strsplit('S121+S12T+S124+S12O+S12Q+S13+S11+S1M+S2+S0+S1+S12K',split='\\+')[[1L]]
+sorder=strsplit('S121+S12T+S124+S12O+S12Q+S13+S11+S1M+S2+S0+S1+S12K+S12R',split='\\+')[[1L]]
 aall[..S2....]=NA; aall[...S2...]=NA
+aall[..S12R....]=NA; aall[...S12R...]=NA
 dall=as.data.table(aall,na.rm=TRUE, simple=TRUE)
 ddn=attr(dall,'dcstruct')
 ddn$REF_SECTOR = ddn$REF_SECTOR[sorder,]
@@ -163,6 +165,8 @@ attr(dall,'dcstruct') = ddn
 aall=as.md3(dall)
 aall[..S2....] = aall[..S0....] - aall[..S1....]
 aall[...S2...] = aall[...S0...] - aall[...S1...]
+aall[..S12R....] = aall[..S124....]+aall[..S12O....]+aall[..S12Q....]
+aall[...S12R...] = aall[...S124...]+aall[...S12O...]+aall[...S12Q...]
 #aall[F511.DK...LE._T.2023q4]
 # Save 
 saveRDS(aall, file.path(data_dir, 'aall1.rds'))
