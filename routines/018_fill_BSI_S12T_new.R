2bc223ab33406d246ce7d862bfce7a23035df990
Stefano Ungaro <83211415+stungaro@users.noreply.github.com>
2025-03-24 14:19:52 +0100

    Update and rename 012_fill_BSI_S12T_new.R to 018_fill_BSI_S12T_new.R

--- routines/018_fill_BSI_S12T_new.R
+++ routines/018_fill_BSI_S12T_new.R
@@ -1,4 +1,3 @@
-library(MDecfin)
 
 # Load assets data
 asbsi= readRDS("data/bsi_ass.rds")
@@ -6,7 +5,7 @@ asbsi= readRDS("data/bsi_ass.rds")
 libsi = readRDS("data/bsi_lia.rds")
 
 #temp load current aall version
-aall=readRDS('data/aall6.rds')
+aall=readRDS('data/aall8.rds')
 setkey(aall, NULL)
 
 
@@ -24,16 +23,11 @@ dimnames(asbsi)
 dimnames(libsi)
 
 
-#function to fill nas
-zerofiller=function(x, fillscalar=0){
-  temp=copy(x)
-  temp[onlyna=TRUE]=fillscalar
-  temp
-}
+
 #check
 aall[F5.AT.S12T..LE._T.2022q4]
 #ASSETS FILLING
-aall[..S12T..._T., usenames=TRUE, onlyna=TRUE] = zerofiller(asbsi["....1998q4:"])
+aall[..S12T..._T., usenames=TRUE, onlyna=TRUE] = asbsi["....1998q4:"]
 #check
 aall[F5.AT.S12T..LE._T.2022q4]
 
@@ -42,8 +36,11 @@ aall[F5.AT.S12T..F._T.2022q4]
 
 
 #LIABILITIES FILLING
-aall[F5..S1.S12T.._T., usenames=TRUE, onlyna=TRUE] = zerofiller(libsi["..1998q4:"])
+aall[F5..S1.S12T.._T., usenames=TRUE, onlyna=TRUE] = libsi["..1998q4:"]
 
 ##### FILL F51M NAS AS RESIDUAL BETWEEN THESE NEW F51 AND POSSIBLY PREEXISTING F511
 gc()
-aall[F51M..S12T..._T., usenames=TRUE, onlyna=TRUE] = zerofiller(aall["F51..S12T..._T."])-zerofiller(aall["F511..S12T..._T."])
+temp=aall["F51..S12T..._T."])-aall["F511..S12T..._T."]
+aall[F51M..S12T..._T., usenames=TRUE, onlyna=TRUE] = temp
+
+saveRDS(aall, file.path(data_dir, 'aall9.rds'))
