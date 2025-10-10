a882aea0e608b75c670bb5492828e158a1691e9e
stungaro <stungaro@gmail.com>
2025-10-10 21:21:11 +0200

    adjustments to loading and filling

--- routines/domestic/009_BSI_F4_F2M_FILL.R
+++ routines/domestic/009_BSI_F4_F2M_FILL.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Load required packages
 library(MDstats)
 library(MD3)
@@ -32,39 +31,4 @@ aall[F2M.AT..S12T.LE._T.2022q4]
 bsi_dep[AT.A.LE..2022q4]
 
 # Check
-=======
-# Load required packages
-library(MDstats)
-library(MD3)
-
-# Define paste0 operator
-`%&%` = function (..., collapse = NULL, recycle0 = FALSE)  .Internal(paste0(list(...), collapse, recycle0))
-
-# Set data directory
-if (!exists("data_dir")) data_dir = getwd()
-
-aall=readRDS(file.path(data_dir, "intermediate_domestic_data_files/aall_bsi_f5.rds"))
-gc()
-setkey(aall, NULL)
-gc()
-bsi_loans_dep= readRDS(file.path(data_dir, "bsi_loans_dep.rds"))
-
-bsi_loans=bsi_loans_dep[".A20....1999q1:"]
-bsi_dep=bsi_loans_dep[".L20....1999q1:"]
-gc()
-
-### fill loans
-names(dimnames(bsi_loans))[4] = 'COUNTERPART_SECTOR'
-aall[F4..S12T..._T.,onlyna=TRUE]<-bsi_loans[.A...]
-aall[F4S..S12T..._T.,onlyna=TRUE]<-bsi_loans[.F...]
-
-### fill deposits
-names(dimnames(bsi_dep))[4] = 'REF_SECTOR'
-aall[F2M...S12T.._T.,onlyna=TRUE]<-bsi_dep[.A...]
-
-aall[F2M.AT..S12T.LE._T.2022q4]
-bsi_dep[AT.A.LE..2022q4]
-
-# Check
->>>>>>> 55a5a789115f3c6e93936ceaded19180b9724739
 saveRDS(aall, file.path(data_dir, 'intermediate_domestic_data_files/aall_bsi_loans_deposits.rds'))
--- routines/loading/006_load_bsi_loans_dep.R
+++ routines/loading/006_load_bsi_loans_dep.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Load required packages
 library(MDstats)
 library(MD3)
@@ -47,54 +46,4 @@ dimnames(bbsiraw)$STO[dimnames(bbsiraw)$STO=='4'] = 'F'
 bbsiraw = bbsiraw[....S12K+S121+S125A+S122+S13+S12Q+S11+S1M.]
 
 bsi_loans_dep=bbsiraw
-=======
-# Load required packages
-library(MDstats)
-library(MD3)
-
-# Set data directory
-if (!exists("data_dir")) data_dir = getwd()
-
-#### BSI LOADING OF BILATERAL LOANS AND DEPOSITS . S12T IS THE REFERENCE SECTOR
-
-# Load quarterly and monthly data (separating Q and M because of widely different elements that are filled)
-bbsirawq=mds('ECB/BSI/Q..N.A.A20+L20.A+F.1+4.U6..Z01.E',labels=TRUE, ccode = NULL)
-bbsirawm=mds('ECB/BSI/M..N.A.A20+L20.A+F.1+4.U6..Z01.E',labels=FALSE, ccode = NULL)
-
-gc()
-
-# Aggregate monthly to quarterly
-bbsiraw=aggregate(bbsirawm,'Q',FUN = end)
-
-gc()
-
-# Rename sectors
-dimnames(bbsiraw)$BS_COUNT_SECTOR[dimnames(bbsiraw)$BS_COUNT_SECTOR=='1000'] = 'S12K'
-dimnames(bbsiraw)$BS_COUNT_SECTOR[dimnames(bbsiraw)$BS_COUNT_SECTOR=='1100'] = 'S121'
-dimnames(bbsiraw)$BS_COUNT_SECTOR[dimnames(bbsiraw)$BS_COUNT_SECTOR=='2271'] = 'S125A'
-dimnames(bbsiraw)$BS_COUNT_SECTOR[dimnames(bbsiraw)$BS_COUNT_SECTOR=='1210'] = 'S122'
-dimnames(bbsiraw)$BS_COUNT_SECTOR[dimnames(bbsiraw)$BS_COUNT_SECTOR=='2100'] = 'S13'
-dimnames(bbsiraw)$BS_COUNT_SECTOR[dimnames(bbsiraw)$BS_COUNT_SECTOR=='2220'] = 'S12Q'
-dimnames(bbsiraw)$BS_COUNT_SECTOR[dimnames(bbsiraw)$BS_COUNT_SECTOR=='2240'] = 'S11'
-dimnames(bbsiraw)$BS_COUNT_SECTOR[dimnames(bbsiraw)$BS_COUNT_SECTOR=='2250'] = 'S1M'
-
-# Rename countries
-dimnames(bbsiraw)$REF_AREA[dimnames(bbsiraw)$REF_AREA=='GB'] = 'UK'
-dimnames(bbsiraw)$REF_AREA[dimnames(bbsiraw)$REF_AREA=='GR'] = 'EL'
-dimnames(bbsiraw)$REF_AREA[dimnames(bbsiraw)$REF_AREA=='U2'] = 'EA20'
-
-# Fill EA data
-bbsiraw['EA19..... ']<- bbsiraw['EA20..... ']
-bbsiraw['EA18..... ']<- bbsiraw['EA20..... ']
-
-# Rename dimensions
-names(dimnames(bbsiraw))[4] = 'STO'
-dimnames(bbsiraw)$STO[dimnames(bbsiraw)$STO=='1'] = 'LE'
-dimnames(bbsiraw)$STO[dimnames(bbsiraw)$STO=='4'] = 'F'
-
-# Filter to relevant sectors
-bbsiraw = bbsiraw[....S12K+S121+S125A+S122+S13+S12Q+S11+S1M.]
-
-bsi_loans_dep=bbsiraw
->>>>>>> 55a5a789115f3c6e93936ceaded19180b9724739
 saveRDS(bsi_loans_dep, file=file.path(data_dir, 'bsi_loans_dep.rds'))
