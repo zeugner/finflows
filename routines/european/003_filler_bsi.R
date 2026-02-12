28af8623c638d7efbd51e3e4eac64c308aaa0ea8
Stefano Ungaro <83211415+stungaro@users.noreply.github.com>
2026-02-12 16:02:55 +0100

    Update  filler_bsi.R
    bug fixing and adaptation to new structure

--- routines/european/003_filler_bsi.R
+++ routines/european/003_filler_bsi.R
@@ -1,29 +1,25 @@
 library(MDstats); library(MD3)
 
 # Set data directory
-#data_dir= file.path(getwd(),'data')
-if (!exists("data_dir")) data_dir = '\\\\s-jrciprnacl01p-cifs-ipsc.jrc.it/ECOFIN/FinFlows/githubrepo/data/'
-
-source('\\\\s-jrciprnacl01p-cifs-ipsc.jrc.it/ECOFIN/FinFlows/githubrepo/finflows/routines/utilities.R')
+data_dir= file.path(getwd(),'data')
 
 
 
 ## load filled iip bop
 ## split of assets and liabilities of banks
-aa=readRDS(file.path(data_dir,'aa_iip_cps.rds')); gc()
-ll=readRDS(file.path(data_dir,'ll_iip_cps.rds')); gc()
+aa=readRDS(file.path(data_dir,'aa_iip_cps_final.rds')); gc()
+ll=readRDS(file.path(data_dir,'ll_iip_cps_final.rds')); gc()
 
 # Load loans data
 
-loans_bsi= readRDS(file.path(data_dir,'bsi_loans.rds')); gc()
+loans_bsi= readRDS(file.path(data_dir, "bsi_loans.rds")); gc()
 
 
-loans_bsi= readRDS("data/bsi_loans.rds")
 dim(loans_bsi)
 dimnames(loans_bsi)[['REF_AREA']] = ccode(dimnames(loans_bsi)[['REF_AREA']],2,'iso2m',leaveifNA=TRUE); gc()
 dimnames(loans_bsi)[['COUNTERPART_AREA']] = ccode(dimnames(loans_bsi)[['COUNTERPART_AREA']],2,'iso2m',leaveifNA=TRUE); gc()
 
-### ?!?! to review = manual allocation of EA to EA20 
+###  manual allocation of EA to EA20 
 dimnames(loans_bsi)$REF_AREA[dimnames(loans_bsi)$REF_AREA=='EA'] ='EA20'
 dimnames(loans_bsi)$COUNTERPART_AREA[dimnames(loans_bsi)$COUNTERPART_AREA=='EA'] ='EA20'
 
@@ -46,14 +42,13 @@ aa[F4.AT.S12T..LE._T.2022q4.]
 
 # Load deposits data
 
-deposits_bsi = readRDS(file.path(data_dir,'bsi_deposits.rds')); gc()
+deposits_bsi = readRDS(file.path(data_dir, "bsi_deposits.rds")); gc()
 
-deposits_bsi = readRDS("data/bsi_deposits.rds")
 dim(deposits_bsi)
 dimnames(deposits_bsi)[['REF_AREA']] = ccode(dimnames(deposits_bsi)[['REF_AREA']],2,'iso2m',leaveifNA=TRUE); gc()
 dimnames(deposits_bsi)[['COUNTERPART_AREA']] = ccode(dimnames(deposits_bsi)[['COUNTERPART_AREA']],2,'iso2m',leaveifNA=TRUE); gc()
 
-### ?!?! to review = manual allocation of EA to EA20 
+###  manual allocation of EA to EA20 
 dimnames(deposits_bsi)$REF_AREA[dimnames(deposits_bsi)$REF_AREA=='EA'] ='EA20'
 dimnames(deposits_bsi)$COUNTERPART_AREA[dimnames(deposits_bsi)$COUNTERPART_AREA=='EA'] ='EA20'
 
@@ -76,11 +71,9 @@ ll[F2M...S12T.LE._T.2022q4.AT]
 
 
 gc()
-saveRDSvl(aa,file.path(data_dir,'aa_iip_bsi.rds'))
-saveRDSvl(ll,file.path(data_dir,'ll_iip_bsi.rds'))
+saveRDS(aa,file.path(data_dir,'aa_iip_bsi.rds'))
+saveRDS(ll,file.path(data_dir,'ll_iip_bsi.rds'))
 
-saveRDSvl(aa,file.path(data_dir,'vintages/aa_iip_bsi_' %&% format(Sys.time(),'%F') %&% '_.rds'))
-saveRDSvl(ll,file.path(data_dir,'vintages/ll_iip_bsi_' %&% format(Sys.time(),'%F') %&% '_.rds'))
+saveRDS(aa,file.path(data_dir,'vintages/aa_iip_bsi_' %&% format(Sys.time(),'%F') %&% '_.rds'))
+saveRDS(ll,file.path(data_dir,'vintages/ll_iip_bsi_' %&% format(Sys.time(),'%F') %&% '_.rds'))
 
-saveRDS(aa,file='data/aa_iip_bsi.rds')
-saveRDS(ll,file='data/ll_iip_bsi.rds')
